b772caf04459376779ea9533a44902a921f2a23a
lamiika <47070356+lamiika@users.noreply.github.com>
2025-02-04 15:25:56 +0200

    Added compression function but final stages are not yet implemented

--- miner.c
+++ miner.c
@@ -17,6 +17,23 @@ typedef struct {
     uint32_t nonce;
 } BlockHeader;
 
+const uint32_t squares[8] = {
+  0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 
+  0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
+};
+
+const uint32_t constants[64] = {
+  0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4,
+  0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe,
+  0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f,
+  0x4a7484aa, 0x5cb0a9dc, 0x76f988da, 0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+  0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc,
+  0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 0xa2bfe8a1, 0xa81a664b,
+  0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070, 0x19a4c116,
+  0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+  0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7,
+  0xc67178f2};
+
 void update_nonce(BlockHeader *header, uint32_t new_nonce) {
   header->nonce = new_nonce;
 }
@@ -103,7 +120,7 @@ void serialize(BlockHeader header, uint8_t *serialized_header) {
   uint32_to_bytes(header.nonce, serialized_header, &iterator);
 }
 
-uint32_t pad(uint8_t *bytes, uint16_t length) {
+uint32_t *pad(uint8_t *bytes, uint16_t length) {
   uint8_t byte = (length+1) / 8 - 1;
   uint8_t extra_bit_spot = (length+1) % 8;
   bytes[byte] = bytes[byte] | (1 << extra_bit_spot);
@@ -114,7 +131,7 @@ uint32_t pad(uint8_t *bytes, uint16_t length) {
     bytes[byte] = 0x00;
   }
 
-  uint32_t message_block[16];
+  static uint32_t message_block[16];
   for (int i = 0; i < 16; i++) {
     message_block[i] = ((uint32_t)bytes[i*4]) |
       ((uint32_t)bytes[i*4 + 1] << 8) |
@@ -150,17 +167,79 @@ uint32_t bitwise1(uint32_t word) {
   return word1 ^ word2 ^ word3;
 }
 
-uint32_t schedule(uint32_t *message_block) {
-  uint32_t message_schedule[64];
+uint32_t bitwise3(uint32_t word) {
+  uint32_t word1 = rotr(word, 2);
+  uint32_t word2 = rotr(word, 13);
+  uint32_t word3 = rotr(word, 22);
+
+  return word1 ^ word2 ^ word3;
+}
+
+uint32_t bitwise4(uint32_t word) {
+  uint32_t word1 = rotr(word, 6);
+  uint32_t word2 = rotr(word, 11);
+  uint32_t word3 = rotr(word, 25);
+
+  return word1 ^ word2 ^ word3;
+}
+
+uint32_t choice(uint32_t x, uint32_t y, uint32_t z) {
+  return (x & y) ^ (~x & z);
+}
+
+uint32_t majority(uint32_t x, uint32_t y, uint32_t z) {
+  return (x & y) ^ (x & z) ^ (y & z);
+}
+
+uint32_t temporary1(uint32_t word, uint32_t constant) {
+  return  ( bitwise4(squares[4]) + choice(squares[4], squares[5], squares[6]) + 
+            squares[7] + constant + word 
+          ) % 0xfffffff;
+}
+
+uint32_t temporary2() {
+  return  ( bitwise3(squares[0]) + 
+            majority(squares[0], squares[1], squares[2]) 
+          ) % 0xfffffff;
+}
+
+uint32_t *schedule(uint32_t *message_block) {
+  static uint32_t message_schedule[64];
 
   for (int i = 0; i < 16; i++) {
     message_schedule[i] = message_block[i];
   }
   for (int i = 16; i < 64; i++) {
-    message_schedule[i] = bitwise1(message_schedule[i-2]) + message_schedule[i-7] + bitwise0(message_schedule[i-15]) + message_schedule[i-16];
+    message_schedule[i] = ( bitwise1(message_schedule[i-2]) + 
+                            message_schedule[i-7] + 
+                            bitwise0(message_schedule[i-15]) + 
+                            message_schedule[i-16] ) % 0xffffffff;
   }
 
   return message_schedule;
+}
+
+
+
+void compression(uint32_t *message_schedule) {
+  uint32_t initial_hash[8];
+  memcpy(initial_hash, squares, sizeof(initial_hash));
+  for (int i = 0; i < 64; i++) {
+    uint32_t word = message_schedule[i];
+    uint32_t constant = constants[i];
+    uint32_t temp1 = temporary1(word, constant);
+    uint32_t temp2 = temporary2();
+
+    for (int j = 7; j <= 0; j--) {
+      initial_hash[i] = initial_hash[i-1]; 
+    }
+    initial_hash[0] = (temp1 + temp2) % 0xffffffff;
+    initial_hash[4] = (initial_hash[4] + temp1) % 0xffffffff; 
+  }
+
+  for (int i = 0; i < 8; i++) {
+    initial_hash[i] = (squares[i] + initial_hash[i]) % 0xffffffff;
+  }
 }
 
 void sha256_hash(uint32_t *message_blocks) {
